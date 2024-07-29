5f8d21c4bc2a892c77e509f712ce86e9d04f42e2
Saola Olaoluwa Feranmi <74037054+Pheranmiey@users.noreply.github.com>
2024-07-29 08:02:46 +0100

    In place reversal of a linked list

--- Reverse Linked List.py
+++ Reverse Linked List.py
@@ -1,6 +1,11 @@
 Input: head = [1,2,3,4,5]
 Output: [5,4,3,2,1]
 
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
 
 def reverse_linked_list(head):
     prev = None
